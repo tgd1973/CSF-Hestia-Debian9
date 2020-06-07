0ccd1abf38e20de2e2af5cb978202bb5201d06e7
tgd1973 <tgd1973@yahoo.com>
2020-06-07 14:04:15 +0200

    Add files via upload

---
+++ Install-hestia.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Made by Steven Sullivan
+# Copyright Steven Sullivan Ltd
+# Version: 1.0
+
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+
+echo "Let's start..."
+
+# Let's install CSF!
+function InstallCSF()
+{
+	echo "Install CSF..."
+	
+	cd /usr/src
+	rm -fv csf.tgz
+	wget https://github.com/tgd1973/CSF-Hestia-Debian/raw/master/csf.tgz
+	tar -xzf csf.tgz
+	cd csf
+	sh install.sh
+}
+
+# Let's install the Hestia / CSF script
+function InstallHestiaCPBashScript()
+{
+	echo "Install HestiaCP Script..."
+	
+	cd /tmp
+	wget -O /usr/local/hestia/bin/v-csf https://vestacp.ss88.uk/Install_CSF_on_VestaCP/v-csf.txt
+	chmod 770 /usr/local/hestia/bin/v-csf
+}
+
+# Let's install the CSF HestiaCP UI!
+function InstallHestiaCPFrontEnd()
+{
+	echo "Install HestiaCP Front..."
+	
+	cd /tmp
+	mkdir /usr/local/hestia/web/list/csf
+	wget https://vestacp.ss88.uk/Install_CSF_on_VestaCP/csf.zip
+	unzip /tmp/csf.zip -d /usr/local/hestia/web/list/
+	rm -f /tmp/csf.zip
+
+	# Chmod files
+	find /usr/local/hestia/web/list/csf -type d -exec chmod 755 {} \;
+	find /usr/local/hestia/web/list/csf -type f -exec chmod 644 {} \;
+	
+	# Add the link to the panel.html file
+	if grep -q 'CSF' /usr/local/hestia/web/templates/admin/panel.html; then
+		echo 'Already there.'
+	else
+		sed -i '/<div class="l-menu clearfix noselect">/a <div class="l-menu__item <?php if($TAB == "CSF" ) echo "l-menu__item--active" ?>"><a href="/list/csf/"><?=__("CSF")?></a></div>' /usr/local/vesta/web/templates/admin/panel.html
+	fi
+}
+
+InstallCSF
+InstallHestiaCPBashScript
+InstallHestiaCPFrontEnd
